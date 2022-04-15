99f2e0214d1ed825fa28af9f0161e46ffc0a1b22
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-15 22:46:48 +0900

    [3-3] Intersection-Observer 특정 요소(이미지 엘리먼트)에 접근할 때만 호출되도록 설정

--- 3-lecture/src/components/Card.js
+++ 3-lecture/src/components/Card.js
@@ -7,9 +7,16 @@ function Card(props) {
 	// 마운트 될 때 한 번만 수행해도됨
 	useEffect(() => {
 		const options = {};
-		const callback = () => {
-			console.log('callback')
-		};
+		const callback =(entries, observer) => {
+			entries.forEach(entry => {
+			  // entry.isIntersecting //  화면 안에 요소가 들어있냐 없냐를 확인
+			  if(entry.isIntersecting) {
+				  console.log('callback', entry.target.dataset.src);
+				  // 이 시점에 img 태그의 src 요소가 생겨, 이때 이미지 리소스를 로드
+				  entry.target.src = entry.target.dataset.src; // 콜백 함수가 호출되면 이미지를 요청!
+			  }
+			});
+		  };
 
 		// 옵저버 객체 등록
 		const observer = new IntersectionObserver(callback, options);
@@ -18,7 +25,7 @@ function Card(props) {
 
 	return (
 		<div className="Card text-center">
-			<img src={props.image} alt="img" ref={imgRef} />
+			<img data-src={props.image} alt="img" ref={imgRef} />
 			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
 				{props.children}
 			</div>
