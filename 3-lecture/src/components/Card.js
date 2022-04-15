a64d0bca4b4adbd66ad5d0e272980b9dd38929e0
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-15 22:17:07 +0900

    [3-3] Intersection-Observer 동작 방식 확인 - I-O 에 간단한 콜백 함수를 등록하여 어떤 상황에서 호출하는 지 확인 - I-O 객체 생성 시점, 등록된 객체 접근 및 나올 때 호출하는 것을 확인

--- 3-lecture/src/components/Card.js
+++ 3-lecture/src/components/Card.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 function Card(props) {
+	const imgRef = useRef(null)
+
+	// 옵저버 기능을 매 순간하는 것이 아닌, 
+	// 마운트 될 때 한 번만 수행해도됨
+	useEffect(() => {
+		const options = {};
+		const callback = () => {
+			console.log('callback')
+		};
+
+		// 옵저버 객체 등록
+		const observer = new IntersectionObserver(callback, options);
+		observer.observe(imgRef.current)	
+	}, [])
+
 	return (
 		<div className="Card text-center">
-			<img src={props.image}/>
+			<img src={props.image} alt="img" ref={imgRef} />
 			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
 				{props.children}
 			</div>
