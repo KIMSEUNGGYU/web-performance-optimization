b28d82d9adde1192128e56993aba946990fdb98f
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-17 06:54:11 +0900

    [3-7] 캐시 최적화 - 리소스별 캐시 적용

--- 3-lecture/server/server.js
+++ 3-lecture/server/server.js
@@ -9,7 +9,18 @@ const header = {
         // res.setHeader('Cache-Control', 'private, no-cache, no-store, must-revalidate')
         // res.setHeader('Expires', '-1')
         // res.setHeader('Pragma', 'no-cache')
-        res.setHeader('Cache-Control', 'max-age: 20')
+        
+        // 모든 리소스에 같은 캐시 설정
+        // res.setHeader('Cache-Control', 'max-age: 20')
+
+        if(path.endsWith('.html')) {
+            res.setHeader('Cache-Control', 'no-cache');
+        } else if(path.endsWith('.js') || path.endsWith('.css') || path.endsWith('.webp')) {
+            // 이미지의 경우 webp 외에 다른 이미지를 사용하지만, 우선적으로 webp 만 처리
+            res.setHeader('Cache-Control', 'public, max-age:31536000');
+        } else {
+            res.setHeader('Cache-Control', 'no-store'); // 'no-cache' 권장
+        }
     },
 }
 
