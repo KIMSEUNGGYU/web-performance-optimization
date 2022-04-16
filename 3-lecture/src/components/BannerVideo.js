f11aeb79065aec72f7e6cb28a32a700af8841a32
kimseunggyu <dev.seunggyu@gmail.com>
2022-04-16 15:19:25 +0900

    [3-6-1] 폰트 최적화 1 - 폰트 적용시점 컨트롤 - font-display 를 이용하여 FOIT 방식으로 폰트를 적용함 - fontObserver 를 이용하여 폰트가로드되는 시점을 파악하여 적용된 폰트가 애니메이션 을 이용하여 컨텐츠가 표시되도록 설정

--- 3-lecture/src/components/BannerVideo.js
+++ 3-lecture/src/components/BannerVideo.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 // import video from '../assets/banner-video.mp4'
 import video_webm from '../assets/_banner-video.webm'
 import video from '../assets/_banner-video.mp4'
 
+import FontFaceObserver from 'fontfaceobserver';
+
 function BannerVideo() {
+	const [isFontLoaded, setIsFontLoaded] = useState(false);
+
+	// font.load 를 렌더링 될 때마다 걸어서 여러 번 호출, 
+	// 그래서 useEffect 로 최초 mouted 될 때 호출되도록 설정
+	useEffect(() => {
+		const font = new FontFaceObserver('BMYEONSUNG');
+		
+		font.load().then(function () {
+			console.log('BMYEONSUNG is available');
+			setIsFontLoaded(true);
+	  	});
+	}, [])
+
 	return (
 		<div className="BannerVideo w-full h-screen overflow-hidden relative bg-texture">
 			<div className="absolute h-screen w-full left-1/2">
@@ -20,7 +35,7 @@ function BannerVideo() {
 					<source src={video} type="video/mp4" />
 				</video>
 			</div>
-			<div className="w-full h-full flex justify-center items-center">
+			<div className="w-full h-full flex justify-center items-center" style={{ opacity: isFontLoaded ? 1 : 0, transition: 'opacity 0.3s ease' }}>
 				<div className="text-white text-center">
 					<div className="text-6xl leading-none font-semibold">KEEP</div>
 					<div className="text-6xl leading-none font-semibold">CALM</div>
